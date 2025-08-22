53ef548193ae281c926e2f05843e5ba46bd09856
Nathan Horn <Natterhorn@gmail.com>
2025-08-22 09:47:21 -0700

    Adding config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+	"port": 8888,
+	"matchData": "data/match.data",
+	"reportsFilename": "data/reports.data",
+	"logFilename": "data/log-DATE.data",
+	"resultsPerPage": 50,
+	"server": "./server/index.js",
+	"contributors": [
+		{
+			"name": "Nate",
+			"html": "",
+			"img": "/img/a.png",
+			"token": "nateisgreat"
+		},
+		// {
+		// 	"name": "Someone",
+		// 	"html": "<p>I add and approve matches, because I care!</p>",
+		// 	"img": "/img/someone.png",
+		// 	"token": "somelse1"
+		// },
+		// {
+		// 	"name": "Yabai",
+		// 	"html": "<p>I don't have a token! I was still useful!</p>",
+		// 	"img": "/img/someone.png"
+		// },
+		// {
+		// 	"name": "HoleeFuk",
+		// 	"html": "<p>I have a token, but can't approve!</p>",
+		// 	"img": "/img/someone.png",
+		// 	"token": "hohoho",
+		// 	"noApprove": true
+		// }
+	]
+};
